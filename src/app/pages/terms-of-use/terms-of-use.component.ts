647b762c8844f7e59faac14e08070768665db1fb
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2026-05-11 17:40:04 -0300

    terms-of-use.component createed

---
+++ src/app/pages/terms-of-use/terms-of-use.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-terms-of-use',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './terms-of-use.component.html',
+  styleUrls: ['./terms-of-use.component.scss']
+})
+export class TermsOfUseComponent {}
